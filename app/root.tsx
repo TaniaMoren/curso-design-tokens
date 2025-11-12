0d0a7d2bf54c9fb4fc083d612745b0f14929cd84
TaniaMoren <taniamoren@gmail.com>
2025-11-12 20:48:03 +0000

    Update fonts

--- app/root.tsx
+++ app/root.tsx
@@ -25,8 +25,12 @@ export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.gstatic.com",
     crossOrigin: "anonymous",
+  },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
   {
     rel: "stylesheet",
@@ -35,8 +39,7 @@ export const links: Route.LinksFunction = () => [
   {
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Bungee+Shade&display=swap",
-  },
-
+  }
 ];
 
 export async function clientAction({ request }: Route.ClientActionArgs) {
